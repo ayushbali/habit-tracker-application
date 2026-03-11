e2db5e19307927c1cf7111c70165deb95a2c6461
Ayush Bali <rishu.baliab@gmail.com>
2026-03-11 13:22:56 +0100

    defined user schema

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+// this model stores user and account information in the database
+
+// imports
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+}, {timestamps: true}); // Automatically adds createdAt and updatedAt fields
+
+export default mongoose.model("User", userSchema);
