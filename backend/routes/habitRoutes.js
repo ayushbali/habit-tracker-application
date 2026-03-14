1d259e000230b77ae910d7f2a6a8e3a23181030f
ayushbali <rishu.baliab@gmail.com>
2026-03-15 00:05:50 +0100

    added put and delete methods

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {createHabit} from '../controllers/habitController.js';
+import {createHabit, getHabits, updateHabit, deleteHabit} from '../controllers/habitController.js';
 
 const router = express.Router();
 
 router.post('/create-habit', createHabit)
+router.get('/habits', getHabits)
+router.put('/habits/:id', updateHabit)
+router.delete('/habits/:id', deleteHabit);
 
 export default router
