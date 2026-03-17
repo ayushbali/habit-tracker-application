3219d0d7e163afac7407b98103e964d55b359b35
ayushbali <rishu.baliab@gmail.com>
2026-03-17 17:57:35 +0100

    added routes for login and delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
-import express from 'express';
+import express from "express";
 
-import { registerUser } from '../controllers/userController.js';
+import {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post('/register-user', registerUser);
+router.post("/register-user", registerUser);
+router.post("/login-user", loginUser);
+router.get("/users", getAllUsers);
+router.delete("/delete-user/:id", deleteUser); // This route will be used by admin to delete a user. It is not exposed to frontend yet, but it is here for future use.
 
 export default router;
