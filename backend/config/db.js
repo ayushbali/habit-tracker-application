9473edec2385a243c7ac77477ebd078e47fadc28
ayushbali <rishu.baliab@gmail.com>
2026-03-17 18:02:21 +0100

    test code for mongoose v5 and earlier

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,13 @@ configDotenv();
 // connectDB function for mongoDB 
 const connectDB = async()=>{
     try {
+        // for mongoose v5 and earlier
+        // const conn = await mongoose.connect(process.env.uri, {
+        //     useNewUrlParser: true,
+        //     useCreateIndex: true,
+        //     useFindAndModify: false
+        // });
+        // for mongoose v6 and later
         const conn = await mongoose.connect(process.env.uri);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } 
