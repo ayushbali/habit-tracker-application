04ba57a799d855238ec615367519cbfe2844aecb
Ayush Bali <rishu.baliab@gmail.com>
2026-03-23 22:06:34 +0100

    no changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,8 +11,8 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register-user", registerUser);
-router.post("/login-user", loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/users", getAllUsers);
 router.delete("/delete-user/:id", authMiddleware, deleteUser); // This route will be used by admin to delete a user. It is not exposed to frontend yet, but it is here for future use.
 
