9e2e31831335aba9bfe7cdc625690523866b2c57
Ayush Bali <rishu.baliab@gmail.com>
2026-03-23 22:05:58 +0100

    implemented nested routes

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { createLog, getLogs, updateLog } from "../controllers/logController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
-const router = express.Router();
+// mergeParams: true allows the router to access the params defined in the parent router (habitRoutes.js) which includes the habitId.
+// This is necessary because the log routes are nested under the habit routes and we need to know which habit the logs belong to..
+const router = express.Router({ mergeParams: true });
 
 router.post("/logs", authMiddleware, createLog);
 router.get("/logs", authMiddleware, getLogs);
