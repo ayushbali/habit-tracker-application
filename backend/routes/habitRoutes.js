4c4b0e836581bbdfe04ad3356960914dd843ec2a
Ayush Bali <rishu.baliab@gmail.com>
2026-03-23 22:05:42 +0100

    implemented nested routes

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -6,8 +6,12 @@ import {
   deleteHabit,
 } from "../controllers/habitController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import logRoutes from "./logRoutes.js";
 
 const router = express.Router();
+
+// Mount log routes under the habit routes. This means that any request to /habits/:habitId/logs will be handled by the logRoutes router. The habitId parameter will be available in the logRoutes router because of the mergeParams: true option we set when creating the logRoutes router.
+router.use("/habits/:habitId", logRoutes);
 
 router.post("/create-habit", authMiddleware, createHabit);
 router.get("/habits", authMiddleware, getHabits);
